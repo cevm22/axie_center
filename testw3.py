15110526d16ec92d04fdab2e0855774f4e197268
CEVM <cevm.12293@hotmail.com>
2022-01-17 19:08:04 -0600

    remove lib and clean code

--- testw3.py
+++ testw3.py
@@ -2,15 +2,12 @@ import logging
 from datetime import datetime
 from json.decoder import JSONDecodeError
 
-import requests
-from requests.packages.urllib3.util.retry import Retry
 from web3 import Web3
 
 from axie_utils.abis import SLP_ABI
 from axie_utils.abis import AXIE_ABI 
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36" # noqa
-TIMEOUT_MINS = 5
 AXIE_CONTRACT = "0x32950db2a7164ae833121501c797d79e7b79d74c"
 AXS_CONTRACT = "0x97a9107c1793bc407d6f527b77e7fff4d812bece"
 SLP_CONTRACT = "0xa8754b9fa15fc18bb59458815510e40a12cd2014"
@@ -18,12 +15,7 @@ WETH_CONTRACT = "0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"
 USDC_CONTRACT = "0x0b7007c13325c48911f73a2dad5fa5dcbf808adc"
 RONIN_PROVIDER_FREE = "https://proxy.roninchain.com/free-gas-rpc"
 RONIN_PROVIDER = "https://api.roninchain.com/rpc"
-RETRIES = Retry(
-    total=5,
-    backoff_factor=2,
-    status_forcelist=[500, 502, 503, 504],
-    allowed_methods=frozenset(['GET', 'POST'])
-)
+
 print("inciando")
 ronin=str('0x1ba2228e2c90bc6cc4fd7c3fe62e796c4321356f')
 
