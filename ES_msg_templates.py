32ce096b780e22f56fc8c202aedd06f2e72ddba7
oficina <cevm.12293@hotmail.com>
2022-01-18 18:34:51 -0600

    add ticket_msg template

--- ES_msg_templates.py
+++ ES_msg_templates.py
@@ -3,16 +3,49 @@ import datetime
 
 axie_url="https://marketplace.axieinfinity.com/axie/"
 owner_url="https://marketplace.axieinfinity.com/profile/"
-
+tx_url="https://explorer.roninchain.com/tx/"
 def ps_msg(msg):
     marketplace=str('[Marketplace]'+'('+str(axie_url+str(msg[1])+')'))
+    
     embed = discord.Embed(title=('**PRIVATE SALE**'),
-    description=("**TICKET ID:** " + str(msg[0])),
-    timestamp=datetime.datetime.utcnow(),
-    color=discord.Color.orange())
-    embed.set_image(url="https://storage.googleapis.com/assets.axieinfinity.com/axies/123456/axie/axie-full-transparent.png")
+                            description=("**TICKET ID:** " + str(msg[0])),
+                            timestamp=datetime.datetime.utcnow(),
+                            color=discord.Color.orange())
+
+    embed.set_image(url="https://storage.googleapis.com/assets.axieinfinity.com/axies/"+str(msg[1])+"/axie/axie-full-transparent.png")
     embed.add_field(name="Axie ID",value=str(msg[1]),inline=True)
     embed.add_field(name="Price",value=('$'+str(msg[2])),inline=True)
     embed.add_field(name="Axie URL",value=(marketplace),inline=True)
+    embed.set_footer(text="PRIVATE SALE")
+    return embed
+
+def ticket_msg(msg):
+    marketplace=str('[Marketplace]'+'('+str(axie_url+str(msg[3])+')'))
+    seller_hash=str("[Seller_Tx]"+'('+str(str(tx_url)+str(msg[5]))+')')
+    buyer_hash=str("[Buyer_Tx]"+'('+str(str(tx_url)+str(msg[7]))+')')
+    AC_to_Seller=str("[AC_to_Seller_Tx]"+'('+str(str(tx_url)+str(msg[10]))+')')
+    AC_to_Buyer=str("[AC_to_Buyer_Tx]"+'('+str(str(tx_url)+str(msg[11]))+')')
+    url_img=str("https://storage.googleapis.com/assets.axieinfinity.com/axies/"+str(msg[3])+"/axie/axie-full-transparent.png")
+    embed = discord.Embed(title=('**PRIVATE SALE - STATUS**'),
+    description=("**TICKET ID:** " + str(msg[0]) + '\n' 
+                + "**STATUS**: " + str(msg[1])
+    ),
+    timestamp=datetime.datetime.utcnow(),
+    color=discord.Color.orange())
+    embed.set_image(url=url_img)    
+    embed.add_field(name="Axie URL",value=(marketplace),inline=True)
+    embed.add_field(name="Price",value=('$'+str(msg[4])),inline=True)
+    embed.add_field(name="Created ",value=str(msg[2]),inline=True)
+    embed.add_field(name="Seller Hash ",value=seller_hash,inline=True)   
+    embed.add_field(name="Seller Status ",value=msg[6],inline=True)
+    embed.add_field(name="Closed",value=msg[12],inline=True)    
+    embed.add_field(name="Buyer Hash ",value=buyer_hash,inline=True)   
+    embed.add_field(name="Buyer Status ",value=msg[8],inline=True)
+    embed.add_field(name="-",value="-",inline=True)
+    embed.add_field(name="AxieCenter Status ",value=msg[9],inline=False)    
+    embed.add_field(name="AC_to_Seller ",value=AC_to_Seller,inline=True)   
+    embed.add_field(name="AC_to_Buyer ",value=AC_to_Buyer,inline=True) 
+    embed.add_field(name="Notes",value=msg[13],inline=False)    
+    
     embed.set_footer(text="PRIVATE SALE")
     return embed
--- axie_center_bot.py
+++ axie_center_bot.py
@@ -25,12 +25,35 @@ async def ps(ctx):
     #template MSG
     vec=[
         "PS-0000001",
-        123456, #axie ID
+        "4164536", #axie ID
         100, #price
-        "4164536", #Axie URL
     ]
     ps_msg_template=ES_msg_templates.ps_msg(vec)
     await ctx.send(embed=ps_msg_template)
+    return
+#=======================
+#Ticket 
+@bot.command()
+async def ticket(ctx):
+    #template MSG TICKET
+    vec=[
+        "PS-0000001",#Ticket ID:
+        "PENDING",#Ticket Status:
+        "2022-01-19 00:00:50",#str(datetime.datetime.utcnow()),#Opened:
+        654321,#Axie ID:
+        100,#Price:
+        "0xcea4ced35f6e5d8ce647099f46d0706ddee5a3d521d169ee3cfbfafa098275c8",#Seller Proof Hash:
+        ":white_check_mark:",#Seller Status Hash:
+        "0xd1fa214b3e920c8d50ab83e502e4237b8de472cb3b3e2a4189d8830fcccedd65",#Buyer Proof Hash:
+        ":x:",#Buyer Status Hash:
+        ":x:",#Assets in AxieCenter: 
+        "0xcea4ced35f6e5d8ce647099f46d0706ddee5a3d521d169ee3cfbfafa098275c8",#AxieCenter to Seller Hash:
+        "0xd1fa214b3e920c8d50ab83e502e4237b8de472cb3b3e2a4189d8830fcccedd65",#AxieCenter to Buyer Hash:
+        ":x:",#Closed:
+        "notas varias en caso de cancelar o error" #Notes
+    ]
+    ticket_msg=ES_msg_templates.ticket_msg(vec)
+    await ctx.send(embed=ticket_msg)
     return
 print("bot started")
 bot.run(config.token)
