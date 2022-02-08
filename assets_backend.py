f47ed6738c6c87516b9c65323aba67d10627ac8f
oficina <cevm.12293@hotmail.com>
2022-02-08 15:24:23 -0600

    typo - add comments to assets_backend.py

--- assets_backend.py
+++ assets_backend.py
@@ -4,11 +4,15 @@ import system_db
 
 def send_assets():
     system_db.reset_ticket_stat_to_pending()
+    # obtener num de ticket_stat = 3 (ready)
     ready=system_db.tickets_pending()
     for i in range(ready):
+        # obtener data del primer ticket con stat 3
         item=system_db.pull_ticket_ready()
-        system_db.update_pass_ticket_ID(item[0])
         print(item)
+        # Cambiar a status PASS = 9, para CASO DE NO COMPLETARSE EL ENVIO DE AC -> OWNERS
+        system_db.update_pass_ticket_ID(item[0])
+        
     print("ready count > "+str(ready))
     return
 
